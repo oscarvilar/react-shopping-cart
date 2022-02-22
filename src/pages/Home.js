18c5970de0ea2e846cf67329ba20ef198d2dbccc
Oscar Vilar <oscar.vilarsalvador@everis.nttdata.com>
2022-02-22 12:45:08 +0100

    tailwind

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Home Page</h1>
+          <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 };
