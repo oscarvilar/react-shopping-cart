016393f6592cd40de501281ee7504deca515dd7b
Oscar Vilar <oscar.vilarsalvador@everis.nttdata.com>
2022-02-22 10:26:19 +0100

    carga inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetail from './pages/ProductDetail';
+
 function App() {
   return (
     <div>
-      
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/products" element={<Products />}></Route>
+        <Route path="/products/:productId" element={<ProductDetail />}></Route>
+      </Routes>
     </div>
   );
 }
