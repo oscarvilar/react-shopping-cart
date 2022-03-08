aa4750b5450bc699244bc17d6a6aca6993e0c0a8
Oscar Vilar <oscar.vilarsalvador@everis.nttdata.com>
2022-03-08 11:04:15 +0100

    cambios responsive

--- src/components/play/Play.js
+++ src/components/play/Play.js
@@ -5,10 +5,16 @@ import playImg from '../../assets/img/play-img.jpg'
 const Play = () => {
   return (
     <div className='full-width playCont'>
-        <div className="drop-shadow">
-            <div className="glass"></div>
-            <span>Descubre qué juegos quieres, desde los éxitos de ventas más exclusivos hasta los indies más innovadores</span>
+      <div className="blur"></div>
+      <div className="text-center content">
+        <h1>Juegos de PlayStation®5</h1>
+        <p>
+          Descubre qué juegos quieres, desde los éxitos de ventas más exclusivos hasta los indies más innovadores, todos ellos cobran vida con la potencia de la consola PS5™.
+        </p>
+        <div>
+          <a href='#' className='btn'>Descubrelos</a>
         </div>
+      </div>
     </div>
   )
 }
