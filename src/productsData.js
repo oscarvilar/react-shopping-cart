d365189441cc381d7d07598d8e27ccbf75429598
Oscar Vilar <oscarbns@gmail.com>
2022-04-05 19:33:39 +0200

    cambios img

--- src/productsData.js
+++ src/productsData.js
@@ -5,7 +5,7 @@ export default [
     "price": 899,
     "description": "HP Victus 16-d0057ns Intel Core i7-11800H/16GB/512GB SSD/RTX 3060/16.1",
     "category": "portatiles",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/1003/10035546/1126-hp-victus-16-d0057ns-intel-core-i7-11800h-16gb-512gb-ssd-rtx-3060-161.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo1.jpg",
     "rating": {
         "rate": 3.9,
         "count": 120
@@ -17,7 +17,7 @@ export default [
     "price": 1100,
     "description": "Asus ROG Strix G15 G513IC-HN004 AMD Ryzen 7 4800H/16GB/512GB SSD/RTX 3050/15.6",
     "category": "portatiles",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/57/576535/1643-asus-rog-strix-g15-g513ic-hn004-amd-ryzen-7-4800h-16gb-512gb-ssd-rtx-3050-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo2.jpg",
     "rating": {
         "rate": 4.1,
         "count": 259
@@ -29,7 +29,7 @@ export default [
     "price": 779,
     "description": "MSI GF63 Thin 11SC-440XES Intel Core i5-11400H/8GB/512GB SSD/GTX 1650/15.6",
     "category": "portatiles",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/80/805204/1553-msi-gf63-thin-11sc-440xes-intel-core-i5-11400h-8gb-512gb-ssd-gtx-1650-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo3.jpg",
     "rating": {
         "rate": 4.7,
         "count": 500
@@ -41,7 +41,7 @@ export default [
     "price": 1899,
     "description": "MSI GP66 Leopard 11UG-089XES Intel Core i7-11800H/32 GB/1TB SSD/RTX 3070/15.6",
     "category": "portatiles",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/82/823774/1323-msi-gp66-leopard-11ug-089xes-intel-core-i7-11800h-32-gb-1tb-ssd-rtx-3070-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo4.jpg",
     "rating": {
         "rate": 2.1,
         "count": 430
@@ -53,7 +53,7 @@ export default [
     "price": 1089,
     "description": "MSI GF63 Thin 11UD-271XES Intel Core i7-11800H/16GB/512GB SSD/RTX 3050Ti/15.6",
     "category": "jewelery",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/76/760044/1893-msi-gf63-thin-11ud-271xes-intel-core-i7-11800h-16gb-512gb-ssd-rtx-3050ti-156-opiniones.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo5.jpg",
     "rating": {
         "rate": 4.6,
         "count": 400
@@ -65,7 +65,7 @@ export default [
     "price": 1119,
     "description": "Acer Nitro 5 AN515-57 Intel Core i7-11800H/16GB/1TB SSD/RTX 3050/15.6",
     "category": "jewelery",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/76/761629/1118-acer-nitro-5-an515-57-intel-core-i7-11800h-16gb-1tb-ssd-rtx-3050-156-3b04f82f-bcfd-44a4-8a13-c8c1011cf3a3.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo2.jpg",
     "rating": {
         "rate": 3.9,
         "count": 70
@@ -77,7 +77,7 @@ export default [
     "price": 1499,
     "description": "Asus ROG G513QM-HF070 AMD Ryzen 9 5900HX/16GB/1TB SSD/RTX3060/15.6",
     "category": "jewelery",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/38/386626/1447-asus-rog-g513qm-hf070-amd-ryzen-9-5900hx-16gb-1tb-ssd-rtx3060-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo3.jpg",
     "rating": {
         "rate": 3,
         "count": 400
@@ -89,7 +89,7 @@ export default [
     "price": 1089,
     "description": "MSI GF63 Thin 11UD-271XES Intel Core i7-11800H/16 GB/512GB SSD/RTX 3050Ti/15.6",
     "category": "jewelery",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/1002/10023603/1390-msi-gf63-thin-11ud-271xes-intel-core-i7-11800h-16-gb-512gb-ssd-rtx-3050ti-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo4.jpg",
     "rating": {
         "rate": 1.9,
         "count": 100
@@ -101,7 +101,7 @@ export default [
     "price": 599,
     "description": "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
     "category": "electronics",
-    "img": "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo5.jpg",
     "rating": {
         "rate": 2.9,
         "count": 250
@@ -112,7 +112,7 @@ export default [
     "price": 599,
     "description": "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
     "category": "electronics",
-    "img": "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo1.jpg",
     "rating": {
         "rate": 2.9,
         "count": 250
@@ -125,7 +125,7 @@ export default [
     "price": 899,
     "description": "HP Victus 16-d0057ns Intel Core i7-11800H/16GB/512GB SSD/RTX 3060/16.1",
     "category": "portatiles",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/1003/10035546/1126-hp-victus-16-d0057ns-intel-core-i7-11800h-16gb-512gb-ssd-rtx-3060-161.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo2.jpg",
     "rating": {
         "rate": 3.9,
         "count": 120
@@ -137,7 +137,7 @@ export default [
     "price": 1100,
     "description": "Asus ROG Strix G15 G513IC-HN004 AMD Ryzen 7 4800H/16GB/512GB SSD/RTX 3050/15.6",
     "category": "portatiles",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/57/576535/1643-asus-rog-strix-g15-g513ic-hn004-amd-ryzen-7-4800h-16gb-512gb-ssd-rtx-3050-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo3.jpg",
     "rating": {
         "rate": 4.1,
         "count": 259
@@ -149,7 +149,7 @@ export default [
     "price": 779,
     "description": "MSI GF63 Thin 11SC-440XES Intel Core i5-11400H/8GB/512GB SSD/GTX 1650/15.6",
     "category": "portatiles",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/80/805204/1553-msi-gf63-thin-11sc-440xes-intel-core-i5-11400h-8gb-512gb-ssd-gtx-1650-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo4.jpg",
     "rating": {
         "rate": 4.7,
         "count": 500
@@ -161,7 +161,7 @@ export default [
     "price": 1899,
     "description": "MSI GP66 Leopard 11UG-089XES Intel Core i7-11800H/32 GB/1TB SSD/RTX 3070/15.6",
     "category": "portatiles",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/82/823774/1323-msi-gp66-leopard-11ug-089xes-intel-core-i7-11800h-32-gb-1tb-ssd-rtx-3070-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo5.jpg",
     "rating": {
         "rate": 2.1,
         "count": 430
@@ -173,7 +173,7 @@ export default [
     "price": 1089,
     "description": "MSI GF63 Thin 11UD-271XES Intel Core i7-11800H/16GB/512GB SSD/RTX 3050Ti/15.6",
     "category": "jewelery",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/76/760044/1893-msi-gf63-thin-11ud-271xes-intel-core-i7-11800h-16gb-512gb-ssd-rtx-3050ti-156-opiniones.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo1.jpg",
     "rating": {
         "rate": 4.6,
         "count": 400
@@ -185,7 +185,7 @@ export default [
     "price": 1119,
     "description": "Acer Nitro 5 AN515-57 Intel Core i7-11800H/16GB/1TB SSD/RTX 3050/15.6",
     "category": "jewelery",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/76/761629/1118-acer-nitro-5-an515-57-intel-core-i7-11800h-16gb-1tb-ssd-rtx-3050-156-3b04f82f-bcfd-44a4-8a13-c8c1011cf3a3.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo2.jpg",
     "rating": {
         "rate": 3.9,
         "count": 70
@@ -197,7 +197,7 @@ export default [
     "price": 1499,
     "description": "Asus ROG G513QM-HF070 AMD Ryzen 9 5900HX/16GB/1TB SSD/RTX3060/15.6",
     "category": "jewelery",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/38/386626/1447-asus-rog-g513qm-hf070-amd-ryzen-9-5900hx-16gb-1tb-ssd-rtx3060-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo3.jpg",
     "rating": {
         "rate": 3,
         "count": 400
@@ -209,7 +209,7 @@ export default [
     "price": 1089,
     "description": "MSI GF63 Thin 11UD-271XES Intel Core i7-11800H/16 GB/512GB SSD/RTX 3050Ti/15.6",
     "category": "jewelery",
-    "img": "https://thumb.pccomponentes.com/w-530-530/articles/1002/10023603/1390-msi-gf63-thin-11ud-271xes-intel-core-i7-11800h-16-gb-512gb-ssd-rtx-3050ti-156.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo4.jpg",
     "rating": {
         "rate": 1.9,
         "count": 100
@@ -221,7 +221,7 @@ export default [
     "price": 599,
     "description": "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
     "category": "electronics",
-    "img": "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+    "img": "https://oscarvilar.com/imagenes/react-shopping-cart/demo5.jpg",
     "rating": {
         "rate": 2.9,
         "count": 250
